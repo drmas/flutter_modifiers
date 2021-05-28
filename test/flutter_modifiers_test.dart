891e31aeb99e3b2897889ec56f62a863faa9cc94
tiandt <hstdt@163.com>
2021-05-28 16:52:47 +0800

    feat: Null safety

--- test/flutter_modifiers_test.dart
+++ test/flutter_modifiers_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_modifiers/modifiers.dart';
+import 'package:flutter_modifiers/flutter_modifiers.dart';
 
 void main() {
   test('.padding', () {
